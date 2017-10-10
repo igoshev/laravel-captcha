08ce161a5fab425fa7fc8bec177f06605dda3ed2
Jorick Schram <jorick@app33.nl>
2017-10-10 20:53:29 +0200

    Add Dutch translation

---
+++ src/resources/lang/vendor/igoshev/nl/captcha.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'enter_code'     => 'Voer de code van de afbeelding in.',
+    'incorrect_code' => 'Ongeldige code van de afbeelding.',
+    'update_code'    => 'Bijwerken',
+];
