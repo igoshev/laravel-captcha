79cd12bb5ab80ed9b80ceca8fe6be31f26c32506
Dmitry Igoshev <dmitry.igo@gmail.com>
2017-10-11 15:32:48 +0300

    update storage

--- src/Captcha/Storage/StorageInterface.php
+++ src/Captcha/Storage/StorageInterface.php
@@ -4,7 +4,18 @@ namespace Igoshev\Captcha\Captcha\Storage;
 
 interface StorageInterface
 {
+    /**
+     * Push the code in the storage.
+     *
+     * @param string $code
+     * @return void
+     */
     public function push($code);
 
+    /**
+     * Pull the code from the storage.
+     *
+     * @return string|null
+     */
     public function pull();
 }
